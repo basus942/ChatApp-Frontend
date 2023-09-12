cc7aa971f23ea30f19183003d7deaa544d6ce64d
Subhajit basu <basus942@gmail.com>
2023-09-13 00:04:31 +0530

    some changes

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "https://chatapp-backend-1bpc.onrender.com",
+  baseURL: "chatapp-backend-1bpc.onrender.com",
   // withCredentials: true,
 });
 
